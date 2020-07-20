d56d4270849d96708c8c084c1391979f2aa5dc1b
higer-deng <460388249@qq.com>
2020-07-20 10:15:10 +0800

    222

--- spring-boot-demo-docker/src/main/java/com/yqsh/springboot/demo/docker/SpringBootDemoDockerApplication.java
+++ spring-boot-demo-docker/src/main/java/com/yqsh/springboot/demo/docker/SpringBootDemoDockerApplication.java
@@ -2,9 +2,18 @@ package com.yqsh.springboot.demo.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoDockerApplication {
+
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello Dorker World !";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoDockerApplication.class, args);
